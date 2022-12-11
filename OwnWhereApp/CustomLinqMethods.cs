6978da0d6bc52cf1f55bcee630f242f57c59fce3
maximphobos <maximphobos@gmail.com>
2022-12-11 12:40:23 +0200

    Add custom where implementation

---
+++ OwnWhereApp/CustomLinqMethods.cs
@@ -0,0 +1,18 @@
+﻿namespace OwnWhereApp
+{
+    public static class CustomLinqMethods
+    {
+        public static IEnumerable<TSource> MyWhere<TSource>(
+            this IEnumerable<TSource> enumerable, 
+            Predicate<TSource> compareMethod)
+        {
+            foreach (TSource item in enumerable)
+            {
+                if (compareMethod(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
